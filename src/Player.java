465846570b5d9e2c30de2fcbacfc2cd4c351c869
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-10-17 20:54:22 +0200

    ændret name's type

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Player {
+    public final String name;
 public Player(int playerNum){
     Scanner scanner = new Scanner(System.in);
-
-
+    System.out.println(playerNum);
+    this.name=scanner.nextLine();
 }
 }
