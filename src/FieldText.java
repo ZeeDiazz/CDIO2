b1a28edaf2d995fc4d2d6f6ae2eafd9bb3962c2e
Zigalow <s224302@dtu.dk>
2022-10-18 01:18:18 +0200

    Testet og fixet FieldText. Testet med parameter '"Dansk"', og ser ud til at virke som det skal. Har også ændret while loop til et for loop

--- src/FieldText.java
+++ src/FieldText.java
@@ -6,29 +6,31 @@ import java.util.Scanner;
 //Buffered reader
 
 public class FieldText {
-        public static void main(String[] args) throws FileNotFoundException {
-                String[] pickLanguage = setPickLanguage("Dansk");
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] pickLanguage = setPickLanguage("Dansk");
 
-                for (int i = 0; i < pickLanguage.length; i++){
-                        System.out.println(pickLanguage[i]);
-                }
+        for (int i = 0; i < pickLanguage.length; i++) {
+            System.out.println(pickLanguage[i]);
+        }
+    }
+
+
+    public static String[] setPickLanguage(String Language) throws FileNotFoundException {
+
+        File danishfile = new File(Language + ".txt");
+        Scanner scan = new Scanner(danishfile);
+
+        //Fordi der er 16 linjer
+        String[] data = new String[16];
+        for (int i = 0; i < data.length; i++) {
+            scan.hasNextLine();
+            data[i] = scan.nextLine();
+            //System.out.println(data[i]);
+
+
         }
 
-        public static String[] setPickLanguage(String Language) throws FileNotFoundException {
 
-                File danishfile = new File(Language + ".txt");
-                Scanner scan = new Scanner(danishfile);
-
-                //Fordi der er 16 linjer
-                String[] data = new String[16];
-                while (scan.hasNextLine()){
-                        int i = 0;
-                        data[i] = scan.nextLine();
-                        //System.out.println(data[i]);
-                        i ++;
-                }
-
-                //return type virker ikke!!
-                return data;
-        }
+        return data;
+    }
 }
