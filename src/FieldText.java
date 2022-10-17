5edcd69a39cc171bfe9ab99273e7d9ef10d89f9a
ZEE <Zaidsh02@gmail.com>
2022-10-17 23:27:40 +0200

    For loop works plus danish file can be read

--- src/FieldText.java
+++ src/FieldText.java
@@ -1,13 +1,32 @@
-import java.io.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//Buffered reader
 
 public class FieldText {
         private String pickLanguage;
 
-        public static String setPickLanguage(String Language){
+        public static void main(String[] args) throws FileNotFoundException {
+                setPickLanguage("Dansk");
+
+
+        }
+
+
+        public static String setPickLanguage(String Language) throws FileNotFoundException {
 
                 if(Language.equals("Dansk") || Language.equals("Danish")){
-                        File file = new File("DanishText.txt");
-                        return "Try";
+                        File danishfile = new File("Dansk.txt");
+                        Scanner scan = new Scanner(danishfile);
+
+
+                        int filenum = (int) danishfile.length();
+                        for(int i = 0; i < 1; i++) {
+                                System.out.println(scan.nextLine());
+                        }
+                        return "Works";
                 }
                 else
                         return "Try Again";
