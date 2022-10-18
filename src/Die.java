4f89208e0022cf459b653d9647ac0939c46d2857
zahed wafa <zahe0026@hotmail.com>
2022-10-18 14:55:35 +0200

    Die klasse (uden interface)

--- src/Die.java
+++ src/Die.java
@@ -1,4 +1,19 @@
-public interface Die {
-    void roll();
-    int getFace();
+//Taget fra CDIO1
+import java.util.Random;
+public class Die {
+
+    private int faceValue;
+    private Random rng;
+
+    //Construktur for Die class.
+
+    public Die() {
+        rng = new Random();
+    }
+    public void roll() {
+        faceValue = rng.nextInt(6) + 1;
+    }
+    public int getFaceValue() {
+        return faceValue;
+    }
 }
