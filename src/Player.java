fd90514408d0fbdba86483ddf4ff1614ddd7f03d
Daniel <danielweper@gmail.com>
2022-10-19 20:40:20 +0200

    Fixed formatting in Player

--- src/Player.java
+++ src/Player.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Player {
     public final String name;
-public Player(int playerNum){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println(playerNum);
-    this.name=scanner.nextLine();
-}
+    public Player(int playerNum){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(playerNum);
+        this.name=scanner.nextLine();
+    }
 }
