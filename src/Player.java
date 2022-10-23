05f6339c98c7bd65b566ee2d8ad15c185e48f10e
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-10-23 17:02:39 +0200

    getAccount

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-
 public class Player {
     public final String name;
+    private int currentBalance;
 public Player(int playerNum){
     Scanner scanner = new Scanner(System.in);
     System.out.println(playerNum);
     this.name=scanner.nextLine();
     //test commit
 }
+public int getAccount() {
+    // forstår ikke hvorfor den ikke 'kalder' på klassen Account
+    return currentBalance; }
 }
+
