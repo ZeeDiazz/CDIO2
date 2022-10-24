b94d70ede87580b8e4f2ce18f3de3be7f0b557a4
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-10-24 15:38:20 +0200

    getAccountFinished

--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,7 @@ public Player(int playerNum){
     Scanner scanner = new Scanner(System.in);
     System.out.println(playerNum);
     this.name=scanner.nextLine();
-    //test commit
+
 }
 public int getAccount() {
     return account.getBalance(); }
