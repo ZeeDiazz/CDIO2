618548d68dc451d8973c270135a57d7c7214c41e
Felix Schmidt <Felix.Blicher.Schmidt@gmail.com>
2022-10-24 16:33:01 +0200

    playerAccount

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Player {
     public final String name;
+    private Account account;
     public Player(int playerNum){
         Scanner scanner = new Scanner(System.in);
         System.out.println(playerNum);
         this.name=scanner.nextLine();
+
     }
+    public int getAccount() {
+        return account.getBalance(); }
 }
