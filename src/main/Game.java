9cb30ec56eac82b72f57a79ac2e32a450255b1d5
Daniel Weper Jensen <danielweper@gmail.com>
2022-10-24 11:11:52 +0200

    Made a primitive game loop in Program.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -3,24 +3,31 @@ package main;
 public class Game {
     private Player[] players;
     private int currentPlayerIndex;
-    private DieCup cup;
     private Field[] fields;
 
     private Player winner;
     private Player loser;
 
-    public Game(Die d1, Die d2) {
+    public Game() {
         this.players = new Player[2];
         this.players[0] = new Player(1);
         this.players[1] = new Player(2);
 
-        this.cup = new DieCup(d1, d2);
         this.fields = new Field[12];
         this.currentPlayerIndex = 0;
     }
 
-    public void playRound() {
-        // TODO: place logic
+    public Field getField(int index) {
+        return this.fields[index];
+    }
+
+    public Player getPlayingPlayer() {
+        return this.players[currentPlayerIndex];
+    }
+
+    public void nextTurn() {
+        Player currentlyPlaying = getPlayingPlayer();
+        // TODO: if player has won, update winner and loser
 
         // Use bithacking to set the index to the next index
         this.currentPlayerIndex = (currentPlayerIndex + 1) & 1;
--- src/main/Program.java
+++ src/main/Program.java
@@ -1,16 +1,53 @@
 package main;
 
+import java.util.Scanner;
+
 public class Program {
+    private static Scanner in;
+
     public static void main(String[] args) {
+        in = new Scanner(System.in);
+
         SixSidedDie d1 = new SixSidedDie();
         SixSidedDie d2 = new SixSidedDie();
-        Game game = new Game(d1, d2);
+        DieCup cup = new DieCup(d1, d2);
+        Game game = new Game();
 
         while (!game.someoneHasWon()) {
-            game.playRound();
+            // Get the player whose turn it is
+            Player currentlyPlaying = game.getPlayingPlayer();
+
+            // Ask the player to roll the dice
+            getPermissionToRoll(currentlyPlaying);
+
+            // Get the result of the dice roll and the corresponding field
+            int rollSum = cup.getSum();
+            Field landedOn = game.getField(rollSum);
+
+            // Inform the player of the place they landed
+            printFieldData(landedOn);
+
+            // Tell the game, the player has ended their turn
+            game.nextTurn();
         }
 
         System.out.println("Congratulations " + game.getWinner() + " you won!");
         System.out.println("Better luck next time " + game.getLoser());
+    }
+
+    private static void getPermissionToRoll(Player player) {
+        String playerInput;
+        while (true) {
+            System.out.println(player.name + " type 'roll' (or 'r') to roll");
+            playerInput = in.nextLine().toLowerCase();
+            if (playerInput.equals("roll") || playerInput.equals("r")) {
+                break;
+            }
+            System.out.println("Ready when you are!");
+        }
+    }
+
+    private static void printFieldData(Field field) {
+        // TODO: print name and maybe description of landed field, as well as the effect it has
     }
 }
