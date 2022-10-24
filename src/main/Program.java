05a661dc9b9e159745713032dbe1e4095823e3dd
Daniel Weper Jensen <danielweper@gmail.com>
2022-10-24 10:51:56 +0200

    Made the main game loop in Program

--- src/main/Program.java
+++ src/main/Program.java
@@ -2,6 +2,15 @@ package main;
 
 public class Program {
     public static void main(String[] args) {
+        SixSidedDie d1 = new SixSidedDie();
+        SixSidedDie d2 = new SixSidedDie();
+        Game game = new Game(d1, d2);
 
+        while (!game.someoneHasWon()) {
+            game.playRound();
+        }
+
+        System.out.println("Congratulations " + game.getWinner() + " you won!");
+        System.out.println("Better luck next time " + game.getLoser());
     }
 }
