aacff8c42150cd437d783997be36971027b9a588
Daniel Weper Jensen <danielweper@gmail.com>
2022-10-28 11:18:23 +0200

    Made account tests, but should be updated if account can never go negative

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,76 @@
+package test.java;
+
+import main.java.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void getBalance_balanceAtZero() {
+        Account acct = new Account(0);
+        assertEquals(0, acct.getBalance());
+    }
+
+    @Test
+    void getBalance_balanceAtSmallPositive() {
+        Account acct = new Account(1000);
+        assertEquals(1000, acct.getBalance());
+    }
+
+    @Test
+    void getBalance_balanceAtBigPositive() {
+        Account acct = new Account(123456789);
+        assertEquals(123456789, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withZero() {
+        Account acct = new Account(1000);
+        acct.updateBalance(0);
+        assertEquals(1000, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withSmallPositive() {
+        Account acct = new Account(1000);
+        acct.updateBalance(50);
+        assertEquals(1050, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withBigPositive() {
+        Account acct = new Account(1000);
+        acct.updateBalance(1234567899);
+        assertEquals(1234568899, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withSmallNegative() {
+        Account acct = new Account(1000);
+        acct.updateBalance(-100);
+        assertEquals(900, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withBigNegative() {
+        Account acct = new Account(12345678);
+        acct.updateBalance(-1234567);
+        assertEquals(11111111, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withNegativeOneBiggerThanInAccount() {
+        Account acct = new Account(2301);
+        acct.updateBalance(-2302);
+        assertEquals(-1, acct.getBalance());
+    }
+
+    @Test
+    void updateBalance_withNegativeALotMoreThanInAccount() {
+        Account acct = new Account(4305);
+        acct.updateBalance(-70000);
+        assertEquals(-65695, acct.getBalance());
+    }
+}
