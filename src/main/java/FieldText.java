18438bad344f6c06e522428be6311b0fad4e4aa1
ZEE <Zaidsh02@gmail.com>
2022-10-28 12:41:36 +0200

    Comments

--- src/main/java/FieldText.java
+++ src/main/java/FieldText.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 
 public class FieldText {
 
-
+    //Denne metode finder filen og går igennem alle linje i filen.
     public static String[] setPickLanguage(String Language) throws FileNotFoundException {
 
         File danishfile = new File(Language + ".txt");
         Scanner scan = new Scanner(danishfile);
 
-        //Fordi der er 22 linjer
+        //Fordi der er 24 linjer
         String[] data = new String[24];
         for (int i = 0; i < data.length; i++) {
             scan.hasNextLine();
